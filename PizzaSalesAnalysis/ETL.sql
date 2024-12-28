0d5279f29b8739ce8a0c77b97b04638503ca03fd
Aditya Singh <aditya.singh@sindia.co.in>
2024-12-29 01:07:44 +0530

    'PizzaAnalysis'

--- PizzaSalesAnalysis/ETL.sql
+++ PizzaSalesAnalysis/ETL.sql
@@ -1,4 +1,5 @@
 #ETL 
+use pizzaanalysis;
 select * from pizza_sales ;
 SELECT 
     SUM(total_price) OVER() 
@@ -33,3 +34,50 @@ set order_date = date_format(
 str_to_date(Replace(order_date,'/','-'), '%d-%m-%Y'),
 '%d-%m-%Y')
 where str_to_date(Replace(order_date,'/','-'),'%d-%m-%Y') is not null ; 
+
+
+select
+	pizza_category,
+	sum(quantity) * 100 /(select sum(quantity) from pizza_sales) as Percentage
+from pizza_sales
+group by 
+	pizza_category ; 
+    
+select
+	pizza_category,
+	sum(quantity) * 100 /(select sum(quantity) from pizza_sales) as Percentage
+from pizza_sales
+where month(str_to_date(order_date, '%d-%m-%Y')) = 1 
+group by 
+	pizza_category ; 
+select * 
+from pizza_sales 
+where month(str_to_date(order_date, '%d-%m-%Y')) = 1 ;
+select
+	pizza_size,
+	sum(quantity) * 100 /(select sum(quantity) from pizza_sales where quarter(str_to_date(order_date, '%d-%m-%Y')) = 1) as Percentage
+from pizza_sales 
+where quarter(str_to_date(order_date, '%d-%m-%Y')) = 1
+group by 
+	pizza_size 
+order by Percentage desc; 
+
+    
+create table pizzaanalysis.pizza_sales_staging
+select * from pizzaanalysis.pizza_sales ; 
+
+select pizza_name,sum(total_price) as Total_revenue
+from pizza_sales 
+group by pizza_name 
+order by Total_revenue desc
+limit 5 ;
+    
+select pizza_name,sum(quantity) as total_quantity
+from pizza_sales 
+group by pizza_name 
+order by total_quantity desc
+limit 5 ;
+    
+# GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'aznpsaznps07' WITH GRANT OPTION;
+# FLUSH PRIVILEGES;
+
